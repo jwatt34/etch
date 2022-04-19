864e908dc161838e870902fef068b8cd5df6f09b
jdwatt <jdwatt@aggies.ncat.edu>
2022-04-19 12:43:47 -0400

    refactor and delete comments

--- app.js
+++ app.js
@@ -4,20 +4,12 @@ const defaultSize = 16;
 let colorChosen = false
 let color = '';
 
-// function setColor(selectedColor) {
-//   colorChosen = true;
-//   color = selectedColor;
-// }
-
 function createGrid(size) {
   let container = document.querySelector('.grid-container');
-  // container.style.setProperty('--grid-rows', size);
-  // container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-  $(container).css('--grid-rows', size);
+$(container).css('--grid-rows', size);
   $(container).css('--grid-cols', size);
   for (let i = 0; i < Math.pow(size, 2); i++) {
-    // let square = document.createElement('div');
-    let square = $('<div />');
+let square = $('<div />');
     $(square).on('mouseover', function() {
       if (color !== 'random') {
         $(this).css('background-color', colorChosen ? color : 'white');
@@ -29,7 +21,6 @@ function createGrid(size) {
       }
     })
     $(square).addClass('box');
-    // container.insertAdjacentElement('beforeend', square);
     $(square).appendTo(container);
   }
 }
@@ -37,13 +28,6 @@ function createGrid(size) {
 function chooseSize(value) {
   createGrid(Number(value));
 }
-
-// function resetGrid() {
-//   // const squares = document.querySelectorAll('.grid-container div');
-//   // squares.forEach(div => $(div).css('background-color', 'white'));
-//   const squares = $('.grid-container div');
-//   $(squares).css('background-color', 'white');
-// }
 
 $('.color-choice').click(function() {
   colorChosen = true;
